7e932b468913521ec9f7644acedff92b9994ca37
Rui Silva <97447789+rpedros02@users.noreply.github.com>
2024-05-20 14:51:37 +0100

    minor error fixes to make code runnable

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/CollaboratorMenuUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/CollaboratorMenuUI.java
@@ -16,14 +16,11 @@ public class CollaboratorMenuUI implements Runnable{
         List<MenuItem> options = new ArrayList<MenuItem>();
         options.add(new MenuItem("Register a new Collaborator", new RegisterCollaboratorUI()));
         options.add(new MenuItem("Register a Job to a Collaborator", new CreateJobUI()));
-        options.add(new MenuItem("Edit a Vehicle", new ShowTextUI("You have chosen Option 3.")));
-        options.add(new MenuItem("Maintenance List", new ShowTextUI("You have chosen Option 4.")));
-        options.add(new MenuItem("Delete a Vehicle", new ShowTextUI("You have chosen Option 5.")));
         options.add(new MenuItem("Exit", new AdminUI()));
 
         int option = 0;
         do {
-            option = Utils.showAndSelectIndex(options, "\n\n--- VEHICLE MENU ------------------------");
+            option = Utils.showAndSelectIndex(options, "\n\n--- COLLABORATOR MENU ------------------------");
 
             if ((option >= 0) && (option < options.size())) {
                 options.get(option).run();
