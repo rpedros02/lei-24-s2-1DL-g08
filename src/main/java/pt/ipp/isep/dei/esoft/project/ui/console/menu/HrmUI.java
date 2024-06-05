0959cd811af8f86b2c5f46851256acbe1bdbc912
ritavares28 <1231261@isep.ipp.pt>
2024-06-05 14:39:13 +0100

    update pprog

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/HrmUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/HrmUI.java
@@ -1,36 +1,4 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
-import pt.ipp.isep.dei.esoft.project.ui.console.*;
-import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class HrmUI implements Runnable {
-    public HrmUI() {
-    }
-
-    /**
-     * Runs the HRM UI.
-     */
-    @Override
-    public void run() {
-        List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Register Skill", new RegisterSkillUI()));
-        options.add(new MenuItem("Register Job", new CreateJobUI()));
-        options.add(new MenuItem("Register Collaborator", new RegisterCollaboratorUI()));
-        options.add(new MenuItem("Assigning Skills to a Collaborator", new AssignSkillUI()));
-        options.add(new MenuItem("Generate Team Proposal", new GenerateTeamProposalUI()));
-        /*options.add(new MenuItem("Option 3", new ShowTextUI("You have chosen Option 3.")));
-        options.add(new MenuItem("Option 4", new ShowTextUI("You have chosen Option 4.")));*/
-
-        int option = 0;
-        do {
-            option = Utils.showAndSelectIndex(options, "\n\n--- HRM MENU -------------------------");
-
-            if ((option >= 0) && (option < options.size())) {
-                options.get(option).run();
-            }
-        } while (option != -1);
-    }
+public class HrmUI {
 }
