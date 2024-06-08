721924dee96b3261b7213026442a93b427c01338
Diogo <133678441+Mkyouser@users.noreply.github.com>
2024-06-08 16:53:19 +0100

    Us22 e 23- Javafx

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/AddEntryToAgendaGUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/AddEntryToAgendaGUI.java
@@ -1,7 +1,6 @@
 package pt.ipp.isep.dei.esoft.project.ui.gui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
