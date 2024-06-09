b0fae33f37ff373d4304a512bca37b571098913d
ritavares28 <1231261@isep.ipp.pt>
2024-06-09 23:46:55 +0100

    CompleteTasksUI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/CompleteTasksUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/CompleteTasksUI.java
@@ -1,10 +1,23 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
+import pt.ipp.isep.dei.esoft.project.application.controller.AgendaController;
+
+import java.util.Scanner;
+
 public class CompleteTasksUI implements Runnable {
+    private final AgendaController agendaController;
+    public CompleteTasksUI() {
+        this.agendaController = new AgendaController();
+    }
 
-        public void run() {
-            System.out.println("Complete Tasks UI");
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the title of the entry to cancel: ");
+        String title = scanner.nextLine();
+
+        boolean result = agendaController.cancelEntry(title);
+        System.out.println(result ? "Entry cancelled successfully." : "Failed to cancel entry.");
 
 
-        }
+    }
 }
